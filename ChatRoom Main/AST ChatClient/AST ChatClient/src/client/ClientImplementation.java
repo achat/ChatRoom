ce3b68d6f3b7588a3cab84ba25e35316168cc1ec
Georgia Arabatzi <46720510+georgifa@users.noreply.github.com>
2019-02-01 23:39:00 +0200

    Update ClientImplementation.java

--- ChatRoom Main/AST ChatClient/AST ChatClient/src/client/ClientImplementation.java
+++ ChatRoom Main/AST ChatClient/AST ChatClient/src/client/ClientImplementation.java
@@ -1,5 +1,28 @@
 package client;
 
-public class ClientImplementation {
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+import chatroominterface.ClientInterface;
+
+public class ClientImplementation extends UnicastRemoteObject implements ClientInterface{
+	private String name;
+
+	/** 
+	 * @param name Client's name. */
+	public ClientImplementation( String name) throws RemoteException{
+		this.name=name;
+	}
+
+	@Override
+	public void sendMessageToClient( String message) throws RemoteException{
+		System.out.println( message);				//to client's console
+	}
+
+	@Override
+	public String getName() throws RemoteException{
+		return name;
+	}
 
 }
