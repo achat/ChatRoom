2fd551bf4784eb182ba625c65fcc3dc6e5586614
Arava <Arava@Jojoba>
2019-02-01 23:21:01 +0200

    added class ClientImp

---
+++ ChatRoom/src/ClientImplementation.java
@@ -0,0 +1,26 @@
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+import chatroominterface.ClientInterface;
+
+public class ClientImplementation extends UnicastRemoteObject implements ClientInterface{
+	private String name;
+
+	/** 
+	 * @param name Client's name. */
+	public ClientImplementation( String name) throws RemoteException{
+		this.name=name;
+	}
+
+	@Override
+	public void sendMessageToClient( String message) throws RemoteException{
+		System.out.println( message);				//to client's console
+	}
+
+	@Override
+	public String getName() throws RemoteException{
+		return name;
+	}
+
+}
