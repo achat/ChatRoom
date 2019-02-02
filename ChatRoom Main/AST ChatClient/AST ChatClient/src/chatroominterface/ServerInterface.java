6f50abe2b4264ecead3cede9f914869471f4a8c5
tasos41 <ttaxiarchou21@gmail.com>
2019-02-02 22:08:22 +0200

    remove comment

--- ChatRoom Main/AST ChatClient/AST ChatClient/src/chatroominterface/ServerInterface.java
+++ ChatRoom Main/AST ChatClient/AST ChatClient/src/chatroominterface/ServerInterface.java
@@ -3,7 +3,6 @@ package chatroominterface;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
-//first commit
 public interface ServerInterface extends Remote {
     public String getMessage() throws RemoteException;
 }
