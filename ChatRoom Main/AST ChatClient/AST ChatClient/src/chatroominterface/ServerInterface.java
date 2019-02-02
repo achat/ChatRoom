fb89578713163de44e47454eea5f65698d2169ef
tasos41 <ttaxiarchou21@gmail.com>
2019-02-02 18:58:22 +0200

    add class ServerInterface

--- ChatRoom Main/AST ChatClient/AST ChatClient/src/chatroominterface/ServerInterface.java
+++ ChatRoom Main/AST ChatClient/AST ChatClient/src/chatroominterface/ServerInterface.java
@@ -3,6 +3,7 @@ package chatroominterface;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
+//first commit
 public interface ServerInterface extends Remote {
     public String getMessage() throws RemoteException;
 }
