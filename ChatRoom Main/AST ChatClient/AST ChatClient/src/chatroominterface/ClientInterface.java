1aa86e104649bc24af090ae86e9028efb626914a
dmetikaridis <dmetikaridis@gmail.com>
2019-02-02 15:36:44 +0200

    Adding the ClientInterface code for the AST ChatClient project.

--- ChatRoom Main/AST ChatClient/AST ChatClient/src/chatroominterface/ClientInterface.java
+++ ChatRoom Main/AST ChatClient/AST ChatClient/src/chatroominterface/ClientInterface.java
@@ -1,5 +1,9 @@
 package chatroominterface;
 
-public interface ClientInterface {
+import java.rmi.Remote;
+import java.rmi.RemoteException;
 
+public interface ClientInterface extends Remote{
+	public void sendMessageToClient( String message) throws RemoteException; 	//Send message to Client.
+	public String getName() throws RemoteException;								//Get Client's name.
 }
