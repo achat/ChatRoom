5e2ca373cc68967c1433abeb8a3a97649daebcff
THEOFILOS <THEOFILOS@DESKTOP-OJA5C6T.lan>
2019-02-03 14:06:59 +0200

    Add a code... (End of sprint 1)

--- ChatRoom Main/AST ChatClient/AST ChatClient/src/client/Client.java
+++ ChatRoom Main/AST ChatClient/AST ChatClient/src/client/Client.java
@@ -1,5 +1,16 @@
 package client;
 
-public class Client {
+import java.rmi.Naming;
 
+import chatroominterface.ServerInterface;
+
+public class Client{
+	public static void main( String...args) throws Exception{
+		ServerInterface serverInteface=( ServerInterface) Naming.lookup( "//localhost/Chatroom");
+		
+		ClientImplementation clientImplementation=new ClientImplementation( "Tester");
+		System.out.println(clientImplementation.getName()+" connected to server.");
+		
+		clientImplementation.sendMessageToClient( "Received from Server: "+ serverInteface.getMessage() );
+	}
 }
