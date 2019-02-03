a9c00f24816fc91194dd639a671931c28d0e2d75
dmetikaridis <dmetikaridis@gmail.com>
2019-02-03 10:34:13 +0200

    Added comments to the client interface

--- ChatRoom Main/AST ChatClient/AST ChatClient/src/chatroominterface/ClientInterface.java
+++ ChatRoom Main/AST ChatClient/AST ChatClient/src/chatroominterface/ClientInterface.java
@@ -2,7 +2,7 @@ package chatroominterface;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
-
+/** Exposing Client's methods to server.*/
 public interface ClientInterface extends Remote{
 	public void sendMessageToClient( String message) throws RemoteException; 	//Send message to Client.
 	public String getName() throws RemoteException;								//Get Client's name.
