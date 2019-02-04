3b905ad812a3bff85386782f6550e5eb6dd64c2d
THEOFILOS <THEOFILOS@DESKTOP-OJA5C6T.lan>
2019-02-04 15:03:58 +0200

    Add a correct code!

--- ChatRoom Main/AST ChatClient/AST ChatClient/src/chatroominterface/ServerInterface.java
+++ ChatRoom Main/AST ChatClient/AST ChatClient/src/chatroominterface/ServerInterface.java
@@ -5,7 +5,6 @@ import java.rmi.RemoteException;
 import java.util.Vector;
 
 public interface ServerInterface extends Remote {
-    public String getMessage() throws RemoteException;
     public boolean login( ClientInterface clientInterface) throws RemoteException;	//Use to login to the chatroom.
 	public boolean logout( ClientInterface clientInterface) throws RemoteException; //Use to logout from the chatroom.
 	public void publish( String message) throws RemoteException; 	 //Send messages to all, clients and server.
