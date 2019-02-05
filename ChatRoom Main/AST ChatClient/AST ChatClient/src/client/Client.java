f479e3da29e0d08e9d0137f98a97eb62c4e8ab60
georgifa <georgifa3@gmail.com>
2019-02-05 15:14:31 +0200

    added a constructor of the client

--- ChatRoom Main/AST ChatClient/AST ChatClient/src/client/Client.java
+++ ChatRoom Main/AST ChatClient/AST ChatClient/src/client/Client.java
@@ -4,13 +4,45 @@ import java.rmi.Naming;
 
 import chatroominterface.ServerInterface;
 
+package client;
+
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.util.Scanner;
+
+import chatroominterface.ServerInterface;
+
+
 public class Client{
-	public static void main( String...args) throws Exception{
-		ServerInterface serverInteface=( ServerInterface) Naming.lookup( "//localhost/Chatroom");
+	private static ServerInterface serverInterface;
+	private ClientImplementation clientImplementation;
+	private String name;
+	
+	public Client(){
+		String ip;
+		Scanner scanner=new Scanner(System.in);
 		
-		ClientImplementation clientImplementation=new ClientImplementation( "Tester");
-		System.out.println(clientImplementation.getName()+" connected to server.");
+		System.out.println("[System] Enter server's ip:");
+		ip=scanner.nextLine();
 		
-		clientImplementation.sendMessageToClient( "Received from Server: "+ serverInteface.getMessage() );
+		System.out.println("[System] Enter your name:");
+		name=scanner.nextLine();
+		
+		try{
+			clientImplementation=new ClientImplementation(name);
+			serverInterface=( ServerInterface) Naming.lookup( "rmi://"+ip+"/Chatroom");
+			serverInterface.login( clientImplementation);	//try to connect to chatroom.
+		}catch( RemoteException | MalformedURLException | NotBoundException exception){
+			exception.printStackTrace();
+		}
+		
 	}
-}
+	
+	public static void main( String...args){
+		System.out.println("[System] Client is running.");
+
+		Client client=new Client();
+				
+	}
