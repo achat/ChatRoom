1f99a4335f1fe4779dba66f2c70eccfba257804f
THEOFILOS <theofilostrichas@gmail.com>
2019-02-06 20:20:59 +0200

    Add a code!

--- ChatRoom Main/AST ChatClient/AST ChatClient/src/client/ClientUserInterface.java
+++ ChatRoom Main/AST ChatClient/AST ChatClient/src/client/ClientUserInterface.java
@@ -30,50 +30,50 @@ public class ClientUserInterface{
 	
 	JTextArea jTextArea;
 	JTextField messageTextfield, ipTextField, nameTextField;
-	JButton connectButton;	//Connect Button
-	JList<String> jList;	//this is the list on the right that contains the users connected to the chatroom
+	JButton connectionButton;	//Connect/Disconnect Button
+	JList<String> jList;		//this is the list on the right that contains the users connected to the chatroom
 	JFrame jFrame;
 	
 	public ClientUserInterface(){
 		jFrame=new JFrame( "Chatroom");
-		JPanel main=new JPanel();
-		JPanel top=new JPanel();
-		JPanel center=new JPanel();
-		JPanel bottom=new JPanel();
+		JPanel panelMain=new JPanel();
+		JPanel panelTop=new JPanel();
+		JPanel panelCenter=new JPanel();
+		JPanel panelBottom=new JPanel();
 		
 		ipTextField=new JTextField();
 		messageTextfield=new JTextField();
 		nameTextField=new JTextField();
 		
 		jTextArea=new JTextArea();
-		connectButton=new JButton( "Connect");
+		connectionButton=new JButton( "Connect");
 		JButton sendButton=new JButton( "Send");
 		jList=new JList<String>();
 		
-		main.setLayout( new BorderLayout( 5, 5) );
-		top.setLayout( new GridLayout( 1, 0, 5, 5) );
-		center.setLayout( new BorderLayout( 5, 5) );
-		bottom.setLayout( new BorderLayout( 5, 5) );
+		panelMain.setLayout( new BorderLayout( 5, 5) );
+		panelTop.setLayout( new GridLayout( 1, 0, 5, 5) );
+		panelCenter.setLayout( new BorderLayout( 5, 5) );
+		panelBottom.setLayout( new BorderLayout( 5, 5) );
 		
-		top.add( new JLabel( "Your name: ") );
-		top.add( nameTextField);
-		top.add( new JLabel( "Server Address: ") );
-		top.add( ipTextField);
-		top.add( connectButton);
+		panelTop.add( new JLabel( "User Name: ") );
+		panelTop.add( nameTextField);
+		panelTop.add( new JLabel( "Server Address: ") );
+		panelTop.add( ipTextField);
+		panelTop.add( connectionButton);
 		
-		center.add( new JScrollPane( jTextArea), BorderLayout.CENTER);
-		center.add( jList, BorderLayout.EAST);
+		panelCenter.add( new JScrollPane( jTextArea), BorderLayout.CENTER);
+		panelCenter.add( jList, BorderLayout.EAST);
 		
-		bottom.add( messageTextfield, BorderLayout.CENTER);
-		bottom.add( sendButton, BorderLayout.EAST);
+		panelBottom.add( messageTextfield, BorderLayout.CENTER);
+		panelBottom.add( sendButton, BorderLayout.EAST);
 		
-		main.add( top, BorderLayout.NORTH);
-		main.add( center, BorderLayout.CENTER);
-		main.add( bottom, BorderLayout.SOUTH);
-		main.setBorder( new EmptyBorder( 10, 10, 10, 10) );
-		
+		panelMain.add( panelTop, BorderLayout.NORTH);
+		panelMain.add( panelCenter, BorderLayout.CENTER);
+		panelMain.add( panelBottom, BorderLayout.SOUTH);
+		panelMain.setBorder( new EmptyBorder( 10, 10, 10, 10) );
+	
 		//Action Listeners for Events.
-		connectButton.addActionListener( new ActionListener(){
+		connectionButton.addActionListener( new ActionListener(){
 			@Override
 			/** When connect button pressed doConnect() */
 			public void actionPerformed( ActionEvent actionEvent){
@@ -97,21 +97,25 @@ public class ClientUserInterface{
 			}
 		});
 	
-		jFrame.setContentPane( main);
+		jFrame.setContentPane( panelMain);
 		jFrame.setSize( 600, 600);
 		jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	//Terminate program when "X" is pressed.
 		jFrame.setLocationRelativeTo(null);						//Place the frame to the screen's center.
 		jFrame.setVisible( true);
 	}	
-	
+
+	/** The actual method to connect/disconnect to/from the server.
+	 * 	When action to connect, checks if name and ip are typed, accesses the server's remote object,
+	 *  logs in and updates the connected users list.
+	 *  When action to disconnect, logs out and updates the connected users list. */
 	public void doConnect(){
-		if( connectButton.getText().equals( "Connect") ){
-			if( nameTextField.getText().length()<2){	//Do not accept names with 1 or less characters.
+		if( connectionButton.getText().equals( "Connect") ){
+			if( nameTextField.getText().length()<2){			//Do not accept names with 1 or less characters.
 				JOptionPane.showMessageDialog( jFrame, "You need to type a name with more than 1 characters.");
 				return;
 			}
 			
-			if( ipTextField.getText().length()<2){		//Do not accept ip with 1 or less characters.
+			if( ipTextField.getText().length()<2){				//Do not accept ip with 1 or less characters.
 				JOptionPane.showMessageDialog( jFrame, "You need to type an IP with more than 1 characters.");
 				return;
 			}
@@ -119,12 +123,12 @@ public class ClientUserInterface{
 			try{
 				clientImplementation=new ClientImplementation( nameTextField.getText() );
 				clientImplementation.setGUI( this);
-
+				
 				serverInterface=( ServerInterface) Naming.lookup( "rmi://"+ipTextField.getText()+"/Chatroom");
 				serverInterface.login( clientImplementation);	//try to connect to chatroom.
 				
 				updateConnectedUsersList( serverInterface.getConnectedClients() ); //add client to clients list.
-				connectButton.setText( "Disconnect");			//change button label to Disconnect.
+				connectionButton.setText( "Disconnect");		//change button label to Disconnect.
 			}catch( Exception exception){
 				exception.printStackTrace();
 				JOptionPane.showMessageDialog( jFrame, "ERROR, we couldn't connect....");
@@ -134,25 +138,25 @@ public class ClientUserInterface{
 				serverInterface.logout( clientImplementation);
 			}catch( RemoteException exception){
 				exception.printStackTrace();
-			}
+			}	
 			updateConnectedUsersList( null);
-			connectButton.setText( "Connect");
-			//TODO: Implement Logout ....
+			connectionButton.setText( "Connect");
 		}
 	}
 
+	/** The actual method to send the message written in the textArea.
+	 * 	Checks if user connected, removes the text form the textArea, concatenates the user name and sends it. */
 	public void sendText(){
-		if( connectButton.getText().equals( "Connect") ){ //Can not send message unless first connected.
+		if( connectionButton.getText().equals( "Connect") ){ 		//Can not send message unless first connected.
 			JOptionPane.showMessageDialog( jFrame, "You need to connect first.");
 			return;
 		}
 		
-		String message=messageTextfield.getText();			//get message from textField.
-		message="["+nameTextField.getText()+"] "+message; 	//add the user's name.
-		messageTextfield.setText( "");						//set textField to empy again.
+		String message=messageTextfield.getText();				//get message from textField.
+		message="["+nameTextField.getText()+"] "+message; 		//add the user's name.
+		messageTextfield.setText( "");							//set textField to empy again.
 
-		//Remove if you are going to implement for remote invocation
-		try{
+		try{ //Publish the message to everyone connected in the chatroom.
 			serverInterface.publish( message);
 		}catch( Exception exception){
 			exception.printStackTrace();
@@ -180,5 +184,6 @@ public class ClientUserInterface{
 			}
 		}
 		jList.setModel( connectedUsersList);
+
 	}
 }
