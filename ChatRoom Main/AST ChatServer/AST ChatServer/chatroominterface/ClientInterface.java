2b87962976fe74a67c24c4b5822ee93b0f074610
dmetikaridis <dmetikaridis@gmail.com>
2019-02-07 08:58:02 +0200

    Updated the interfaces in the Server project.

--- ChatRoom Main/AST ChatServer/AST ChatServer/chatroominterface/ClientInterface.java
+++ ChatRoom Main/AST ChatServer/AST ChatServer/chatroominterface/ClientInterface.java
@@ -2,8 +2,11 @@ package chatroominterface;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.util.Vector;
 
 public interface ClientInterface extends Remote{
 	public void sendMessageToClient( String message) throws RemoteException; 	//Send message to Client.
 	public String getName() throws RemoteException;								//Get Client's name.
+	//Update the connected users jList.
+	public void updateConnectedUsersList(Vector<ClientInterface> vector) throws RemoteException;
 }
--- ChatRoom Main/AST ChatServer/AST ChatServer/chatroominterface/ServerInterface.java
+++ ChatRoom Main/AST ChatServer/AST ChatServer/chatroominterface/ServerInterface.java
@@ -4,9 +4,10 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.util.Vector;
 
-public interface ServerInterface extends Remote {
+public interface ServerInterface extends Remote{
     public boolean login( ClientInterface clientInterface) throws RemoteException;	//Use to login to the chatroom.
-	public boolean logout( ClientInterface clientInterface) throws RemoteException; //Use to logout from the chatroom.
+	public boolean logout( ClientInterface clientInterface) throws RemoteException; //Use to logout from chatroom.
 	public void publish( String message) throws RemoteException; 		//Send messages to clients and to server.
 	public Vector<?> getConnectedClients() throws RemoteException;	 	//Get all clients connected to the chatroom.
+	public String getServerPublicIP() throws RemoteException;	 		//Get the server's public ip.
 }
