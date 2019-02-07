371f907fcedf54b64eb3c9824e57be944c751315
dmetikaridis <dmetikaridis@gmail.com>
2019-02-07 17:50:53 +0200

    New methods added to help implement the features forementioned.

--- ChatRoom Main/AST ChatServer/AST ChatServer/server/ServerImplementation.java
+++ ChatRoom Main/AST ChatServer/AST ChatServer/server/ServerImplementation.java
@@ -1,6 +1,11 @@
 package server;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
 import java.rmi.RemoteException;
+import java.rmi.server.ServerNotActiveException;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.Vector;
 
@@ -9,19 +14,27 @@ import chatroominterface.ServerInterface;
 
 public class ServerImplementation extends UnicastRemoteObject implements ServerInterface{
 	private Vector<ClientInterface> vector=new Vector<ClientInterface>();	//A vector that holds connected clients.
-	public ServerImplementation() throws RemoteException{} 	//Do not remove this.
+	private ServerDatabase serverDatabase;
+	
+	public ServerImplementation(ServerDatabase serverDatabase) throws RemoteException{ //Do not remove this.
+		this.serverDatabase=serverDatabase;
+	} 	
 	
 	@Override
 	/** The Client uses this method to connect to the server. */
 	public boolean login( ClientInterface clientInterface) throws RemoteException{
-		//System.out.println("[System] "+ clientInterface.getName()+" got connected...");
-		
+	
 		//Inform client for successful connection.
 		clientInterface.sendMessageToClient( "[System] You have connected successfully to the chatroom.");
+		
 		//Inform all clients that a new client has connected.
 		publish("[System] "+ clientInterface.getName()+" has just connected."); 
 		
+		if (serverDatabase!=null)							//update only if database enabled.
+			updateLogsTable("connection",clientInterface);
+		
 		vector.add( clientInterface); 						//add client to client list.
+		updateClientsUI();									//Update the client jList on connected users.
 		return true;										//true for successful connection.
 	}
 
@@ -32,19 +45,24 @@ public class ServerImplementation extends UnicastRemoteObject implements ServerI
 		//Inform client for successful disconnection.
 		clientInterface.sendMessageToClient( "[System] You have disconnected successfully from the chatroom.");
 		
+		if (serverDatabase!=null)							//update only if database enabled.
+			updateLogsTable("connection",clientInterface);
+		
 		vector.remove( clientInterface); 					//Remove client from client list before publishing.
 		
 		//Inform all clients that the client has disconnected.
 		publish("[System] "+ clientInterface.getName()+" has just disconnected."); 
-		
+			
+		updateClientsUI();									//Update the client jList on connected users.
 		return true;										//true for successful disconnection.
 	}
-	
+
 	@Override
 	/** Publish the message to all users in the chatroom and to the server's black screen.
 	 * @param message The message to send. */
 	public void publish( String message) throws RemoteException{
-		System.out.println( message);			//This will write the message to the server's black screen.
+		if (!message.contains( "[Admin]") )
+			System.out.println( message);				//This will write the message to the server's black screen.
 		
 		for( int i=0; i<vector.size(); i++){	//Send to all users
 			try{
@@ -60,6 +78,45 @@ public class ServerImplementation extends UnicastRemoteObject implements ServerI
 	/** Get all clients connected to the chatroom.*/
 	public Vector<ClientInterface> getConnectedClients() throws RemoteException{
 		return vector;
+	}
+	
+	@Override
+	/** Get the server's public IP. */
+	public String getServerPublicIP() throws RemoteException{
+		String publicIP=null;
+		
+		try{
+			URL url=new URL( "http://checkip.amazonaws.com");
+			BufferedReader bufferedReader=new BufferedReader( new InputStreamReader( url.openStream() ) );
+			publicIP=bufferedReader.readLine(); 	//Get the public IP as a String
+		}catch( IOException exception){
+			exception.printStackTrace();
+		}
+
+		return publicIP;
+	}
+	
+	/** The method to update the Logs table in the database.
+	 * @param action The action performed by the user, either connection or disconnection.
+	 * @param clientInterface The user who performed this action. */
+	private void updateLogsTable(String action, ClientInterface clientInterface){
+		try{	//Update chatroom_logs table in database.
+			serverDatabase.updateLogsTable( "connection", getClientHost(), clientInterface.getName() );
+		}catch( ServerNotActiveException | RemoteException exception){
+			exception.printStackTrace();
+		}
+	}
+	
+	/** This method should be called when a user logs in/out to update the other users jList on their GUI. */
+	private void updateClientsUI(){
+		for( int i=0; i<vector.size(); i++){			//Update connected users list according to logins/logouts
+			try{
+				ClientInterface clientInterface=vector.get( i);
+				clientInterface.updateConnectedUsersList(vector);
+			}catch( Exception exception){
+				exception.printStackTrace();
+			}
+		}
 	}
 	
 }
